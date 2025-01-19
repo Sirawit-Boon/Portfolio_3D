c3d7edb146e2ae2e08d0c58ad4f1869384b11757
Sirawit-Boon <ter.sirawit1997@gmail.com>
2025-01-19 15:49:59 +0700

    Initail commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto border border-red-500">
+      <h1 className="text-2xl text-white underline">Hello Three</h1>
+    </main>
+  );
+};
+
+export default App;
